4d7e2444dbbf4985202206f4ec666f2280d65345
Enjoy3325 <enjoy3325@gmail.com>
2023-04-24 12:37:43 +0300

    hw-02 done

--- App.js
+++ App.js
@@ -1,37 +1,21 @@
 
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { RegistrationScreens } from './Screens/RegistrationScreen'
-// import { AppLoading } from 'expo';
 import { StyleSheet, View } from 'react-native';
 
-// import { useFonts } from "expo-font";
-// import * as SplashScreen from "expo-splash-screen";
-// import * as Font from 'expo-font';
-// import React,{ useState } from 'react';
+import RegistrationScreens  from './Screens/RegistrationScreen'
+// import LoginScreen from './Screens/LoginScreen';
+
 export default function App() {
-  // const loadFonts = async () => {
-  // await Font.loadAsync({
-  //   "Roboto": require("./assets/fonts/Regular.ttf"),
-
-  // });
-  // };
-
-
-  // const [isReady, setIsReady] = useState(false)
-  // if (!isReady) {
-  //   return <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} />
-  // }
 
   return (
 
     <View style={styles.container}>
 
-      <StatusBar style="auto" />
+      <RegistrationScreens />
+      {/* <LoginScreen/> */}
+          <StatusBar style="auto" />
 
-   
-       
-        <RegistrationScreens />
-      
     </View>
 
   );
@@ -40,24 +24,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    fontWeight: 400,
-      marginBottom: 0,
-    backgroundColor: '#fff',
-
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    // textAlign: 'center',
   },
-  imageBackground: {
-    flex: 1,
-    resizeMode: 'cover',
-    height:812,
-    // justifyContent: 'center',
-    justifyContent: 'flex-end',
-    // marginBottom: 78,
-
-    // alignItems: 'center',
-  },
-
-
 });
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react"
-import { useCallback } from 'react';
+import React, { useState, useCallback } from "react"
 import {
     KeyboardAvoidingView,
     SafeAreaView,
@@ -9,34 +8,31 @@ import {
     Text,
     TouchableOpacity,
     Keyboard,
+    Platform,
+    ImageBackground,
     Image,
     TouchableWithoutFeedback,
-    Dimensions,
 } from "react-native"
+
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
+SplashScreen.preventAutoHideAsync();
 
 const initialState = {
-    login: '',
-    email: '',
-    password: '',
+    login: "",
+    email: "",
+    password: "",
 }
 
 export const RegistrationScreens = () => {
+    const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+    const [state, setState] = useState(initialState);
+
     const [fontsLoaded] = useFonts({
-        'Inter': require('../assets/fonts/Inter.ttf'),
-        'Roboto-Bold': require('../assets/fonts/Roboto-Bold.ttf'),
-        'Roboto-Medium': require('../assets/fonts/Roboto-Medium.ttf'),
-        'Roboto-Regular': require('../assets/fonts/Roboto-Regular.ttf'),
+        "Roboto-Regular": require('../assets/fonts/Roboto-Regular.ttf'),
     });
 
-    const [isShowKeyboard, setIsShowKeyboard] = useState(false);
-    const [state, setState] = useState(initialState)
-    const keyboardHide = () => {
-        setIsShowKeyboard(false)
-        Keyboard.dismiss()
-    }
     const onLayoutRootView = useCallback(async () => {
         if (fontsLoaded) {
             await SplashScreen.hideAsync();
@@ -46,112 +42,142 @@ export const RegistrationScreens = () => {
     if (!fontsLoaded) {
         return null;
     }
-    return (
-        <TouchableWithoutFeedback onPress={keyboardHide}>
-            <View style={styles.container} onLayout={onLayoutRootView}>
 
-                <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-                    <View style={{ ...styles.form, paddingBottom: isShowKeyboard ? 78 : 32 }}>
-                        <View style={styles.avatarWrapper}>
-                            <View>
-                                <Image source={require('../assets/images/add.svg')}></Image>
+    const keyboardHide = () => {
+        setIsShowKeyboard(false);
+        Keyboard.dismiss();
+        console.log(state);
+        setState(initialState);
+    };
+
+    return (
+        <TouchableWithoutFeedback onPress={() => keyboardHide()}>
+            <View style={styles.container} onLayout={onLayoutRootView}>
+                <ImageBackground style={styles.image} source={require("../assets/images/bg.jpg")}>
+                    <KeyboardAvoidingView
+                        behavior={Platform.OS === 'ios' ? 'padding' : ''}>
+                        <View style={{ ...styles.containerForm, marginBottom: isShowKeyboard ? -120 : 0 }}>
+                            <View style={styles.avatarWrapper}>
+                                <Image />
+                                <Image source={require("../assets/images/add.png")} style={styles.iconAdd}></Image>
+                            </View>
+                            <View style={styles.form}>
+                                <View>
+                                    <Text style={styles.title}>Registration</Text>
+                                </View>
+                                    <View>
+                                        <TextInput style={styles.input}
+                                            onFocus={() => setIsShowKeyboard(true)}
+                                            onChangeText={(value) => setState(prevState => ({ ...prevState, login: value }))}
+                                            textAlign={'left'}
+                                            value={state.login}
+                                            placeholderTextColor={'#BDBDBD'}
+                                            placeholder="Login" />
+
+                                        <TextInput style={styles.input}
+                                            onFocus={() => setIsShowKeyboard(true)}
+                                            onChangeText={(value) => setState(prevState => ({ ...prevState, email: value }))}
+                                            textAlign={'left'}
+                                            value={state.email}
+                                            placeholderTextColor={'#BDBDBD'}
+                                            placeholder="E-mail address" />
+                                    </View>
+
+                                    <View>
+                                        <TextInput style={styles.input}
+                                            onFocus={() => setIsShowKeyboard(true)}
+                                            onChangeText={(value) => setState(prevState => ({ ...prevState, password: value }))}
+                                            textAlign={'left'}
+                                            value={state.password}
+                                            placeholderTextColor={'#BDBDBD'}
+                                            placeholder="Password"
+                                            secureTextEntry={true} />
+                                        <Text style={styles.textInputPassword}>Show</Text>
+                                    </View>
+
+                                <View />
+                                <TouchableOpacity onPress={keyboardHide}
+                                    activeOpacity={0.8} style={styles.button} >
+                                    <Text style={styles.textButton}>Register</Text>
+                                </TouchableOpacity>
+                                <View>
+                                    <Text style={styles.subTitleAuth}>Do you already have an account? To come in</Text>
+                                </View>
                             </View>
                         </View>
-
-                        <Text style={styles.text}>Регистрация</Text>
-                        <SafeAreaView style={styles.inputView}>
-                            <TextInput style={styles.input}
-                                onFocus={() => setIsShowKeyboard(true)}
-                                onChangeText={(value) => setState(prevState => ({ ...prevState, login: value }))}
-                                textAlign={'left'}
-                                value={state.login}
-                                placeholderTextColor={'#BDBDBD'}
-                                placeholder="Логин" />
-                            <TextInput style={styles.input}
-                                onFocus={() => setIsShowKeyboard(true)}
-                                onChangeText={(value) => setState(prevState => ({ ...prevState, email: value }))}
-                                textAlign={'left'}
-                                value={state.email}
-                                placeholderTextColor={'#BDBDBD'}
-                                placeholder="Адрес электронной почты" />
-                            <View style={styles.boxInputPassword}>
-                                <TextInput style={styles.input}
-                                    onFocus={() => setIsShowKeyboard(true)}
-                                    onChangeText={(value) => setState(prevState => ({ ...prevState, password: value }))}
-                                    textAlign={'left'}
-                                    value={state.password}
-                                    placeholderTextColor={'#BDBDBD'}
-                                    placeholder="Пароль"
-                                    secureTextEntry={true} />
-                                <Text style={styles.textInputPassword}>Показать</Text>
-                            </View>
-
-                        </SafeAreaView>
-                        <TouchableOpacity onPress={keyboardHide} activeOpacity={0.8} style={styles.button} >
-                            <Text style={styles.textButton}>Зарегистрироваться</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.subTitleAuth}>Уже есть аккаунт? Войти</Text>
-
-                    </View>
-                </KeyboardAvoidingView>
-
+                    </KeyboardAvoidingView> 
+                </ImageBackground>
             </View>
         </TouchableWithoutFeedback>
-    )
-}
+    );
+};
+
+export default RegistrationScreens;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        marginBottom: 0,
-        backgroundImage: URL('../assets/images/bg.png'),
-        resizeMode: 'cover',
-        justifyContent: 'flex-end',
+// justifyContent: 'center',
     },
 
-    imageBackground: {
+    image: {
         flex: 1,
         resizeMode: 'cover',
-        height: 812,
-        // justifyContent: 'center',
         justifyContent: 'flex-end',
-        // marginBottom: 78,
-
-        // alignItems: 'center',
+        
     },
-    form: {
-        // flex:s,
-        // position: 'relative',
-        height: 549,
-        //    height:'67.611%',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        // justifyContent: 'center',
-        // justifyContent: 'flex-start',
+    title: {
+        fontFamily: "Roboto-Regular",
+        fontSize: 30,
+        lineHeight: 35,
+        letterSpacing: 0.01,
+        textAlign: "center",
+        marginBottom: 33,
+        color: "#212121",
+    },
+
+    containerForm: {
+        paddingTop: 92,
         borderTopLeftRadius: 25,
         borderTopRightRadius: 25,
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff",
+    },
+    form: {
         marginHorizontal: 16,
-        // paddingBottom: 78,
-        // paddingTop:92,
-        //  paddingBottom: 33,
-        // marginBottom:78,
+    },
+
+    input: {
+        fontFamily: "Roboto-Regular",
+        fontSize: 16,
+        lineHeight: 19,
+        height: 50,
+        padding: 16,
+        marginTop: 16,
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: "#e8e8e8",
+        backgroundColor: "#f6f6f6",
+        color: "#212121",
+    },
+
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        height: 51,
+        marginTop: 43,
+        marginBottom: 16,
+        borderRadius: 100,
+        backgroundColor: "#ff6c00",
     },
 
     avatarWrapper: {
-        // position: 'absolute',
-        // justifyContent: 'center',
-        top: '-7%',
-        left: '0%',
-        // marginTop:32,
+        position: "absolute",
+        left: "35%",
+        top: "-15%",
         width: 120,
         height: 120,
-        backgroundColor: '#F6F6F6',
-        borderBottomEndRadius: 16,
-        borderBottomStartRadius: 16,
-        borderTopEndRadius: 16,
-        borderTopStartRadius: 16,
+        borderRadius: 16,
+        backgroundColor: "#f6f6f6",
 
     },
     text: {
@@ -163,66 +189,40 @@ const styles = StyleSheet.create({
         marginBottom: 33,
         color: '#212121',
     },
-    inputView: {
-        marginBottom: 27,
 
 
-    },
-    boxInputPassword: {
-        // position: 'relative',
-    },
-    input: {
-        fontFamily: 'Roboto',
-        width: 343,
-        height: 50,
-        fontSize: 18,
-        borderWidth: 1,
-        borderColor: '#E8E8E8',
-        backgroundColor: '#F6F6F6',
-        borderRadius: 8,
-        // marginRight: 'auto',
-        // marginLeft: 'auto',
-        padding: 16,
-        marginBottom: 16,
-        color: '#212121',
-
-    },
     textInputPassword: {
-        fontFamily: 'Roboto',
-        position: 'absolute',
-        top: '22%',
-        right: '5%',
+        fontFamily: "Roboto-Regular",
+        position: "absolute",
+        top: "48%",
+        left: "85%",
         fontSize: 16,
         lineHeight: 19,
-        color: '#1B4371',
+        color: "#1b4371",
     },
 
-    button: {
-        width: 343,
-        backgroundColor: '#FF6C00',
-        borderRadius: 100,
-        // marginLeft: 'auto',
-        // marginRight: 'auto',
-        // marginHorizontal: 35,
-        marginBottom: 16,
-    },
+
     textButton: {
-        fontFamily: 'Roboto',
-        textAlign: 'center',
-        paddingTop: 16,
-        paddingBottom: 16,
-        // paddingLeft: 'auto',
-        // paddingRight: 'auto',
+        fontFamily: "Roboto-Regular",
         fontSize: 16,
         lineHeight: 19,
-        color: '#ffffff',
+        color: "#ffffff",
     },
     subTitleAuth: {
-        fontFamily: 'Roboto',
+        fontFamily: "Roboto-Regular",
         fontSize: 16,
         lineHeight: 19,
         textAlign: 'center',
         color: '#1B4371',
-        // marginBottom: 66,
+        marginBottom: 32,
     },
-})
+
+    iconAdd: {
+        position: "absolute",
+        left: "89%",
+        top: "65%",
+        width: 25,
+        height: 25,
+        // color: "#FF6C00",
+    },
+});
